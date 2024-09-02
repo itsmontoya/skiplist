f24b539daa325fc7f45b0bf0d2653f2c20ffa9be
Josh Montoya <josh@montoya.io>
2024-09-02 13:54:34 -0700

    implement utils

---
+++ utils.go
@@ -0,0 +1,18 @@
+package bssl
+
+func findEnd(bs []byte) (i int) {
+	var b byte
+	for i, b = range bs {
+		if b == 0 {
+			return
+		}
+	}
+
+	i++
+	return
+}
+
+func toString(bs []byte) (str string) {
+	i := findEnd(bs)
+	return string(bs[:i])
+}
