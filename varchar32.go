0ad77f09b2df5616844af4935a4337e38b9b41ee
Josh Montoya <josh@montoya.io>
2024-09-04 11:16:59 -0700

    implement custom comparison func for Varchar32

--- varchar32.go
+++ varchar32.go
@@ -1,7 +1,5 @@
 package skiplist
 
-import "slices"
-
 var _ Key = Varchar32{}
 
 func MakeVarchar32(str string) (out Varchar32) {
@@ -17,5 +15,16 @@ func (v Varchar32) String() string {
 
 func (v Varchar32) Compare(in any) (result int) {
 	b := *(in.(*Varchar32))
-	return slices.Compare(v[:], b[:])
+	for i := 0; i < 32; i++ {
+		aV := v[i]
+		bV := b[i]
+		switch {
+		case aV < bV:
+			return -1
+		case aV > bV:
+			return 1
+		}
+	}
+
+	return 0
 }
